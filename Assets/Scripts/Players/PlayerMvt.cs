c04c2c04aadf30cd9f0d8a6f024cb8755b4f1275
tymmi78 <timothee.gallier@outlook.fr>
2025-01-08 23:49:24 +0100

    Debut mvt du perso implémenté

--- Assets/Scripts/Players/PlayerMvt.cs
+++ Assets/Scripts/Players/PlayerMvt.cs
@@ -1,25 +1,27 @@
 using UnityEngine;
 
-public class player_mvt
+public class PlayerMvt : MonoBehaviour
 {
     public int speed;
     private Rigidbody2D rb;
     public int JumpPower;
-    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
+    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            rb.linearVelocity =new Vector2(-speed, rb.linearVelocity.y);
+            rb.linearVelocity =new Vector2(-speed * 2, rb.linearVelocity.y);
         }
         if (Input.GetKeyDown(KeyCode.D))
         {
-            rb.linearVelocity = new Vector2(speed, rb.linearVelocity.y);
+            rb.linearVelocity = new Vector2(speed * 2, rb.linearVelocity.y);
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
