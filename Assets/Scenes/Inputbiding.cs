1570e93122a6d091455cf2ab7b4e4eee7a5b4ddf
david chau <david.chau@epita.fr>
2025-01-11 18:46:07 +0100

    input binding + base item

--- Assets/Scenes/Inputbiding.cs
+++ Assets/Scenes/Inputbiding.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using Input = UnityEngine.Windows.Input;
 
@@ -8,7 +9,7 @@ public class InputBinding : MonoBehaviour
 {
     [SerializeField] private InputInfos[] inputs;
     private Dictionary<string, char> inputsDictionnary = new Dictionary<string, char>();
-
+    [SerializeField] private TMP_Text buttonText;
     private string BindingAxis = "";
     void Start()
     {
@@ -29,6 +30,12 @@ public class InputBinding : MonoBehaviour
                     if (UnityEngine.Input.GetKey(_keycode))
                     {
                         inputsDictionnary[BindingAxis] = (char)_keycode;
+
+                        if (buttonText != null)
+                        {
+                            buttonText.text = _keycode.ToString();
+                        }
+
                         BindingAxis = "";
                         return;
                     }
@@ -40,9 +47,12 @@ public class InputBinding : MonoBehaviour
             TestInput(_inputAxis);
         }
     }
-    
-    public void Bind(string _axis) => BindingAxis = _axis;
 
+    public void Bind(string _axis) { BindingAxis = _axis;}
+
+    public void BindButton(TMP_Text button)  { buttonText = button; }
+        
+   
     private void TestInput(string _inputAxis)
     {
         bool _input = UnityEngine.Input.GetKey((KeyCode)inputsDictionnary[_inputAxis]);
@@ -50,6 +60,19 @@ public class InputBinding : MonoBehaviour
         {
             Debug.Log(_inputAxis + " : " + inputsDictionnary[_inputAxis]);
         }
+    }
+    public bool IsInputActive(string inputAxis)
+    {
+        if (inputsDictionnary.ContainsKey(inputAxis))
+        {
+            return UnityEngine.Input.GetKey((KeyCode)inputsDictionnary[inputAxis]);
+        }
+        return false;
+    }
+
+    public Dictionary<string, char> GetInputsDictionary()
+    {
+        return new Dictionary<string, char>(inputsDictionnary);
     }
 }
 
