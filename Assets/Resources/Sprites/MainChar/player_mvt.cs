d7ff65f4c87339a56ddc6f4943c8ed172533265d
Dark <dark.event.fr@gmail.com>
2025-01-11 23:37:08 +0100

    rm .idea

--- Assets/Resources/Sprites/MainChar/player_mvt.cs
+++ Assets/Resources/Sprites/MainChar/player_mvt.cs
@@ -1,30 +1,30 @@
-namespace DefaultNamespace;
-using UnityEngine;
-
-public class player_mvt
-{
-    public int speed;
-    private Rigidbody2D rb;
-    public int JumpPower;
-    
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            rb.linearVelocity =new Vector2(-speed, rb.linearVelocity.y);
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            rb.linearVelocity = new Vector2(speed, rb.linearVelocity.y);
-        }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.AddForce(Vector2.up * JumpPower, ForceMode2D.Impulse);
-        }
-    }
-}
+// namespace DefaultNamespace;
+// using UnityEngine;
+//
+// public class player_mvt
+// {
+//     public int speed;
+//     private Rigidbody2D rb;
+//     public int JumpPower;
+//     
+//     void Start()
+//     {
+//         rb = GetComponent<Rigidbody2D>();
+//     }
+//
+//     void Update()
+//     {
+//         if (Input.GetKeyDown(KeyCode.Q))
+//         {
+//             rb.linearVelocity =new Vector2(-speed, rb.linearVelocity.y);
+//         }
+//         if (Input.GetKeyDown(KeyCode.D))
+//         {
+//             rb.linearVelocity = new Vector2(speed, rb.linearVelocity.y);
+//         }
+//         if (Input.GetKeyDown(KeyCode.Space))
+//         {
+//             rb.AddForce(Vector2.up * JumpPower, ForceMode2D.Impulse);
+//         }
+//     }
+// }
